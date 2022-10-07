60d8b88f627a009734b2b824a0fb819743705b7f
Chewby Irinine <al_kor_00@bk.ru>
2022-10-07 04:34:20 +0300

    Адаптивный навбар

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,42 @@
 <template>
   <nav class="navbar is-success" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <RouterLink to="/" class="navbar-item is-size-4 is-family-monospace">
-        eclipse-currency-conversion
-      </RouterLink>
-    </div>
-    <div id="navbarBasicExample" class="navbar-menu">
-      <div class="navbar-end">
-        <RouterLink to="/" class="navbar-item" active-class="is-active"> Home </RouterLink>
-        <RouterLink to="/about" class="navbar-item" active-class="is-active"> About </RouterLink>
+    <div class="container is-max-desktop">
+      <div class="navbar-brand">
+        <RouterLink to="/" class="navbar-item is-size-4 is-family-monospace">
+          eclipse-currency-conversion
+        </RouterLink>
+        <button
+          :class="{ 'is-active': showMobileNav }"
+          class="navbar-burger"
+          @click="showMobileNav = !showMobileNav"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </button>
+      </div>
+      <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': showMobileNav }">
+        <div class="navbar-end">
+          <RouterLink to="/" class="navbar-item" active-class="is-active"> Home </RouterLink>
+          <RouterLink to="/about" class="navbar-item" active-class="is-active"> About </RouterLink>
+        </div>
       </div>
     </div>
   </nav>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const showMobileNav = ref(false);
+</script>
+
+<style lang="scss" scoped>
+@media screen and (max-width: 1023px) {
+  .navbar-menu {
+    position: absolute;
+    left: 0;
+    width: 100%;
+  }
+}
+</style>
