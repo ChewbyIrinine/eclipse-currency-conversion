304a33927e05d5f17d1eba642ec994078ed28d48
Chewby Irinine <al_kor_00@bk.ru>
2022-10-07 04:13:31 +0300

    Установил bulma, добавил навбар

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navbar is-success" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <RouterLink to="/" class="navbar-item is-size-4 is-family-monospace">
+        eclipse-currency-conversion
+      </RouterLink>
+    </div>
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-end">
+        <RouterLink to="/" class="navbar-item" active-class="is-active"> Home </RouterLink>
+        <RouterLink to="/about" class="navbar-item" active-class="is-active"> About </RouterLink>
+      </div>
+    </div>
+  </nav>
+</template>
