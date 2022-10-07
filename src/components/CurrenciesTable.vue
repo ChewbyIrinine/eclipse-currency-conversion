b49e9dd63baf7ebc3f53d7184057d8d9f72d317d
Chewby Irinine <al_kor_00@bk.ru>
2022-10-07 10:56:13 +0300

    Добавил компонент ячейки курса с цветовым обозначением

--- src/components/CurrenciesTable.vue
+++ src/components/CurrenciesTable.vue
@@ -1,30 +1,29 @@
 <template>
   <table class="table is-fullwidth">
     <thead>
-    <tr>
-      <th>Number code</th>
-      <th>Character code</th>
-      <th>Nominal</th>
-      <th>Name</th>
-      <th>Value</th>
-      <th>Previous</th>
-    </tr>
+      <tr>
+        <th>Number code</th>
+        <th>Character code</th>
+        <th>Nominal</th>
+        <th>Name</th>
+        <th>Exchange rate</th>
+      </tr>
     </thead>
     <tbody>
-    <tr v-for="currency in currenciesStore.getFilteredCurrencies" :key="currency.ID">
-      <td>{{ currency.NumCode }}</td>
-      <td>{{ currency.CharCode }}</td>
-      <td>{{ currency.Nominal }}</td>
-      <td>{{ currency.Name }}</td>
-      <td>{{ currency.Value }}</td>
-      <td>{{ currency.Previous }}</td>
-    </tr>
+      <tr v-for="currency in currenciesStore.getFilteredCurrencies" :key="currency.ID">
+        <td>{{ currency.NumCode }}</td>
+        <td>{{ currency.CharCode }}</td>
+        <td>{{ currency.Nominal }}</td>
+        <td>{{ currency.Name }}</td>
+        <td><ExchangeRateCell :value="currency.Value" :previous="currency.Previous" /></td>
+      </tr>
     </tbody>
   </table>
 </template>
 
 <script setup>
 import { useCurrenciesStore } from '@/stores/currenciesStore';
+import ExchangeRateCell from '@/components/ExchangeRateCell.vue';
 
 const currenciesStore = useCurrenciesStore();
 </script>
---
+++ src/components/ExchangeRateCell.vue
@@ -0,0 +1,24 @@
+<template>
+  <span :style="{ color: getTextColor }">{{ value.toFixed(4) }} ({{ getDifference }})</span>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  value: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+  previous: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+});
+
+const getTextColor = computed(() => (props.value >= props.previous ? 'green' : 'red'));
+
+const getDifference = computed(() => (props.value - props.previous).toFixed(1));
+</script>
